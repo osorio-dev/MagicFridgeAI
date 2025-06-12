1bdddff12b62ef984e80a57f0d5cd6fd7f27e5b9
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-11 23:16:16 -0300

    first commit

---
+++ src/test/java/com/dev/osorio/MagicFridgeAI/MagicFridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.osorio.MagicFridgeAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicFridgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
