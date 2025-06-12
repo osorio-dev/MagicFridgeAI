dddf22f03a3548484a902425ccbc8db2986ecefd
osorio-dev <feeh.osorio.dev@gmail.com>
2025-06-12 16:54:16 -0300

    feat: create and implement repository for food_item

--- src/main/java/com/dev/osorio/MagicFridgeAI/Repository/FoodItemRepository.java
+++ src/main/java/com/dev/osorio/MagicFridgeAI/Repository/FoodItemRepository.java
@@ -1,4 +1,9 @@
 package com.dev.osorio.MagicFridgeAI.Repository;
 
-public class FoodItemRepository {
+import com.dev.osorio.MagicFridgeAI.Model.FoodItemModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodItemRepository extends JpaRepository<FoodItemModel, Long> {
 }
